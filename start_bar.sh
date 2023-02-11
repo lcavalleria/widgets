519bcd69c5c588f651c68c395cfcabbf223361ed
lcavalleria <llcav92@gmail.com>
2023-02-11 14:41:53 +0100

    improve taskbar script for multiscreen

--- start_bar.sh
+++ start_bar.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
+# if MAIN_MONITOR_NAME is not set, main monitor will be the first of the wayland list.
+# MAIN_MONITOR_NAME will be checked by grep, so any substring in wlrctl outputs will work.
 
-#user=$(whoami)
-# export necessary variables to call eww daemon and + wlrctl
-#export XDG_RUNTIME_DIR="/run/user/$(id -u $user)"
-#export XDG_CONFIG_HOME="/home/$user/.config"
-#export WAYLAND_DISPLAY="wayland-1"
-### 
-displays=$(wlrctl output list | wc -l)
-eww close bar1
+IFS=$'\n'
+mapfile -t displays_arr < <(wlrctl output list)
+main_display=$(printf '%s\n' \"${displays_arr[@]}\" | grep -n "$MAIN_MONITOR_NAME" | cut -c 1)
 eww close bar0
-if [ $displays -gt 1 ]; then
-	eww open bar1
+eww close bar1
+eww close bar2
+if [ ${#displays_arr[@]} -gt 1 ]; then
+	if [ $main_display -eq 3 ]; then
+		eww open bar2
+	elif [ $main_display -eq 2 ]; then
+		eww open bar1
+	else
+		eww open bar0
+	fi
 else
 	eww open bar0
 fi
+unset IFS
